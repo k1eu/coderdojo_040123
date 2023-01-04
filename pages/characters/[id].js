f1e3ac2f889f19d92afbf997119332bdc71b5999
Tomasz Kielar <git@k1eu.dev>
2023-01-04 20:25:02 +0100

    a

---
+++ pages/characters/[id].js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+const CharacterPage = ({ character }) => {
+  console.log(character)
+  return (
+    <main>
+      <h1>Character {character.name}</h1>
+      <h2>Spiecies {character.species}</h2>
+      <img src={character.image} alt={character.name} />
+    </main>
+  )
+}
+
+export default CharacterPage
+
+export async function getStaticPaths() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+
+  const characters = data.results;
+
+  const paths = characters.map((character) => ({
+    params: { id: character.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      character: data,
+    },
+  };
+}
+
+
